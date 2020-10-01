2100078568d1777e1eb2ffb39bb23ac4f92bbf73
Elvin Dzhavadov <elvin.d@outlook.com>
2020-10-01 18:59:06 +0300

    update dependencies

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ["eslint-config-airbnb", "plugin:@typescript-eslint/recommended"],
+  extends: ['eslint-config-airbnb', 'plugin:@typescript-eslint/recommended'],
   plugins: [
-    "@typescript-eslint",
-    "prettier",
-    //"react-hooks"
+    '@typescript-eslint',
+    'prettier',
+    // "react-hooks"
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     commonjs: true,
@@ -14,75 +14,86 @@ module.exports = {
     node: true,
   },
   rules: {
-    "indent": "off",
+    indent: 'off',
     // both are find https://eslint.org/docs/rules/arrow-body-style#top
-    "arrow-body-style": "off",
+    'arrow-body-style': 'off',
     // Incompatible with prettier
-    "implicit-arrow-linebreak": "off",
+    'implicit-arrow-linebreak': 'off',
     // leave it for prettier
-    "comma-dangle": "off",
+    'comma-dangle': 'off',
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.ts', '.tsx'] }],
-    "no-nested-ternary": "off",
-    // incompatible using redux, if there will be options disable only for imports I would definitely use it
-    "no-shadow": "off",
-    "no-implicit-coercion": "error",
+    'no-nested-ternary': 'off',
+    // incompatible using redux, if there will be options disable only for imports
+    // I would definitely use it
+    'no-shadow': 'off',
+    'no-implicit-coercion': 'error',
     // prettier
-    "no-confusing-arrow": "off",
-    "template-curly-spacing" : "error",
-    "no-unused-vars": "warn",
-    "prettier/prettier": [
-      "error",
+    'no-confusing-arrow': 'off',
+    'template-curly-spacing': 'error',
+    'no-unused-vars': 'warn',
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
-        arrowParens: "avoid"
-      }
+        arrowParens: 'avoid',
+      },
     ],
-    "operator-linebreak": [
-      "error",
-      "after",
-      { overrides: { "?": "ignore", ":": "ignore" } }
+    'operator-linebreak': [
+      'error',
+      'after',
+      { overrides: { '?': 'ignore', ':': 'ignore' } },
     ],
-    "arrow-parens": ["error", "as-needed"],
-    "object-curly-newline": ["error", { consistent: true }],
-    "function-paren-newline": "off",
-    "react/no-unused-state": "warn",
-    "react/prefer-stateless-function": ["warn", { ignorePureComponents: true }],
-    "react/jsx-wrap-multilines": "off",
-    "react/jsx-one-expression-per-line": "off",
-    "react/jsx-first-prop-new-line": "off",
-    "react/jsx-indent": "off",
+    'arrow-parens': ['error', 'as-needed'],
+    'object-curly-newline': ['error', { consistent: true }],
+    'function-paren-newline': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    'react/no-unused-state': 'warn',
+    'react/prefer-stateless-function': ['warn', { ignorePureComponents: true }],
+    'react/jsx-wrap-multilines': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-first-prop-new-line': 'off',
+    'react/jsx-indent': 'off',
     // prettier conflict
-    "react/jsx-curly-newline": "off",
+    'react/jsx-curly-newline': 'off',
     // prettier conflict
-    "space-before-function-paren": "off",
+    'space-before-function-paren': 'off',
     // use ts
     'react/require-default-props': 'off',
-    "react/prop-types": "off",
+    'react/prop-types': 'off',
     'react/jsx-props-no-spreading': 'off',
 
-    "react/jsx-no-duplicate-props": ["error", { ignoreCase: false }],
+    'react/jsx-no-duplicate-props': ['error', { ignoreCase: false }],
 
-    "jsx-a11y/label-has-associated-control": "off",
-    "react/jsx-sort-props": ["warn", {
-      ignoreCase: true
-    }],
-    "react/jsx-sort-default-props": ["warn", {
-      ignoreCase: true
-    }],
-    "react/sort-prop-types": ["warn", {
-      ignoreCase: true
-    }],
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        ignoreCase: true,
+      },
+    ],
+    'react/jsx-sort-default-props': [
+      'warn',
+      {
+        ignoreCase: true,
+      },
+    ],
+    'react/sort-prop-types': [
+      'warn',
+      {
+        ignoreCase: true,
+      },
+    ],
     'react/state-in-constructor': 'off',
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "warn",
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'warn',
     // prettier
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/interface-name-prefix": ["error", "always"],
+    '@typescript-eslint/indent': 'off',
     // don't like it anymore
     'react/destructuring-assignment': 'off',
 
-    //"react-hooks/rules-of-hooks": "error",
-    //"react-hooks/exhaustive-deps": "warn"
+    // "react-hooks/rules-of-hooks": "error",
+    // "react-hooks/exhaustive-deps": "warn"
 
     'import/extensions': [
       'error',
@@ -96,14 +107,14 @@ module.exports = {
     ],
   },
   settings: {
-    "import/extensions": [".js",".jsx",".ts",".tsx"],
-    "import/parsers": {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
-  }
-}
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ test/index.ts
@@ -0,0 +1,5 @@
+export const fn = (arg: number) => arg + 1;
+
+export function fn1(arg: number) {
+  return arg + 1;
+}
