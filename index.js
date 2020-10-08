ce2b3e13c09609accfb993c4a983c184652613cd
Elvin Dzhavadov <elvin.d@outlook.com>
2020-10-08 21:10:23 +0300

    better configure prettier

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['eslint-config-airbnb', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint-config-airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
   plugins: [
     '@typescript-eslint',
     'prettier',
@@ -14,22 +20,12 @@ module.exports = {
     node: true,
   },
   rules: {
-    indent: 'off',
-    // both are find https://eslint.org/docs/rules/arrow-body-style#top
-    'arrow-body-style': 'off',
-    // Incompatible with prettier
-    'implicit-arrow-linebreak': 'off',
-    // leave it for prettier
-    'comma-dangle': 'off',
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.ts', '.tsx'] }],
     'no-nested-ternary': 'off',
     // incompatible using redux, if there will be options disable only for imports
     // I would definitely use it
     'no-shadow': 'off',
     'no-implicit-coercion': 'error',
-    // prettier
-    'no-confusing-arrow': 'off',
-    'template-curly-spacing': 'error',
     'no-unused-vars': 'warn',
     'prettier/prettier': [
       'error',
@@ -44,20 +40,9 @@ module.exports = {
       { overrides: { '?': 'ignore', ':': 'ignore' } },
     ],
     'arrow-parens': ['error', 'as-needed'],
-    'object-curly-newline': ['error', { consistent: true }],
-    'function-paren-newline': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-
     'react/no-unused-state': 'warn',
     'react/prefer-stateless-function': ['warn', { ignorePureComponents: true }],
-    'react/jsx-wrap-multilines': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/jsx-first-prop-new-line': 'off',
-    'react/jsx-indent': 'off',
-    // prettier conflict
-    'react/jsx-curly-newline': 'off',
-    // prettier conflict
-    'space-before-function-paren': 'off',
     // use ts
     'react/require-default-props': 'off',
     'react/prop-types': 'off',
@@ -87,8 +72,6 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'import/prefer-default-export': 'off',
     'import/no-default-export': 'warn',
-    // prettier
-    '@typescript-eslint/indent': 'off',
     // don't like it anymore
     'react/destructuring-assignment': 'off',
 
